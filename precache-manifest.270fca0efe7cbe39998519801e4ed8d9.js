10d74b301b0fff613bc98c00743d2aeb08092d80
Louis Daza <louisdaza@hotmail.com>
2018-12-13 12:44:16 -0800

    Updates

---
+++ precache-manifest.270fca0efe7cbe39998519801e4ed8d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/foo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ef2cec3ff3a955317a37",
+    "url": "/foo/static/js/runtime~main.ef2cec3f.js"
+  },
+  {
+    "revision": "82a4cece0e54991d6dd8",
+    "url": "/foo/static/js/main.82a4cece.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/foo/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "82a4cece0e54991d6dd8",
+    "url": "/foo/static/css/main.8e89ae4b.chunk.css"
+  },
+  {
+    "revision": "519e2e25e7c67f0d5b4f8511429d87b5",
+    "url": "/foo/index.html"
+  }
+];
